b1e43eb31278ec228bba702a95c8164c36e8c3e4
Vitor Olivera Santos <vitorosantos15@gmail.com>
2020-01-27 17:37:35 -0300

    Updates

---
+++ precache-manifest.eec062627c1b979f54e6be81a0bd831b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c253fcc3371f543d0c417e5ea14fded",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "d1f9cfc39c947dce2dc9",
+    "url": "/task-manager/static/css/main.57877ec2.chunk.css"
+  },
+  {
+    "revision": "c02f7350f21610c1d99a",
+    "url": "/task-manager/static/js/2.661a4390.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/task-manager/static/js/2.661a4390.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1f9cfc39c947dce2dc9",
+    "url": "/task-manager/static/js/main.e085673e.chunk.js"
+  },
+  {
+    "revision": "f2e97961f022f03ee229",
+    "url": "/task-manager/static/js/runtime-main.e3354537.js"
+  }
+]);
